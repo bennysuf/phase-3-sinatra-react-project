ee84465a97ed7ce8a7e1f26dffb16db2424ce384
Benny Sufrin <bennysuf@gmail.com>
2023-02-15 12:21:05 -0500

    finishes with controllers, adds some logic to models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  
   # USER 
   get "/login" do
     user = User.all
     user.to_json
   end
 
-  post "/login/new-user" do #work
+  post "/login/new-user" do 
     user = User.create(
       username: params[:username],
       password: params[:password]
@@ -17,25 +15,20 @@ class ApplicationController < Sinatra::Base
     user.to_json
   end
 
-  # patch "/edit-user/:id" do
-  #   user = User.find(params[:id])
-  #   user.update(
-  #     username: params[:username]
-  #   )
-  #   user.to_json
-  # end
+  patch "/edit-user/:id" do
+    user = User.find(params[:id])
+    user.update(
+      username: params[:username]
+    )
+    user.to_json
+  end
 
-  # delete "/edit-user/:id" do 
-  #   user = User.find(params[:id])
-  #   post = Post.find_by(user_id: params[:id])
-  #   comment = Comment.find_by(user_id: params[:id])
-  #   user.destroy
-  #   post.destroy
-  #   comment.destroy
-  #   user.to_json
-  #   post.to_json
-  #   comment.to_json
-  # end
+  delete "/edit-user/:id" do 
+    user = User.find(params[:id])
+    check = Post.checkPosts(user.posts.all)
+    user.destroy.to_json
+    check.to_json
+  end
 
   get "/user/:id" do
     user = User.find(params[:id])
@@ -49,9 +42,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/posts/:id" do
-    self_post = Post.find_by(user_id: params[:id])
-    # self_post.posts.all
-    self_post.to_json
+    user = User.find(params[:id])
+    user.posts.all.to_json
   end
 
 
@@ -62,14 +54,6 @@ class ApplicationController < Sinatra::Base
     )
     post.to_json
   end
-
-#   delete "/posts/delete/:id" do
-#     post = Post.find(params[:id])
-#     post.destroy
-#     post.to_json
-# end
-
-  # COMMENT
 
   # CURRENT USER
   patch "/current-user/:id" do
@@ -84,10 +68,11 @@ class ApplicationController < Sinatra::Base
     user.to_json
   end
 
-  # delete "/current-user" do 
-  #   user = CurrentUser.all.destroy
-  #   user.to_json
-  # end
+  delete "/current-user" do 
+    user = CurrentUser.first
+    user.destroy
+    user.to_json
+  end
 
 
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ActiveRecord::Base
     belongs_to :user
+
+    def self.checkPosts(post) 
+        if post != nil
+            post.each do |t| 
+                t.destroy.to_json
+            end
+        end
+    end
+
+
 end
